aefcebb8ec1cfcac18ffd7bb9b7fdfe7bb3a8adc
Nada Feteiha <nada.feteiha@gmail.com>
2025-07-25 11:57:44 -0700

    update redpoly

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -18,20 +18,35 @@ else
 fi
 
 cd $PORTFOLIO_DIR
+
+echo "Bringing down any running Docker containers..."
+docker compose -f docker-compose.prod.yml down
+
+echo "********************************************"
+echo "Rebuilding and bringing up Docker containers..."
+docker compose -f docker-compose.prod.yml up -d --build
+
+echo "********************************************"
+echo "Deployment complete! portfolio is now running."
+
+
 # same step to run VM
-echo "********************************************"
-echo "setting up virtual environment..."
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
+# echo "********************************************"
+# echo "setting up virtual environment..."
+# python -m venv python3-virtualenv
+# source python3-virtualenv/bin/activate
 
-# install req
-echo "********************************************"
-echo "installing requirements..."
-pip install -r requirements.txt
+# # install req
+# echo "********************************************"
+# echo "installing requirements..."
+# pip install -r requirements.txt
 
-echo "********************************************"
-echo "Starting services"
-systemctl restart myportfolio.service
+# echo "********************************************"
+# echo "Starting services"
+# systemctl restart myportfolio.service
 
-echo "********************************************"
-echo "portfolio redeployed successfully!"
+# echo "********************************************"
+# echo "portfolio redeployed successfully!"
+
+
+
